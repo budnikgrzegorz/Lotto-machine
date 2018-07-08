41ee7288eb031f15e33fc9efe14e472705226e50
Grzegorz_B <budnikgrzegorz@interia.pl>
2018-07-08 18:14:55 +0200

    Initial commit

---
+++ src/sample/Calculations.java
@@ -0,0 +1,37 @@
+package sample;
+
+import java.util.LinkedList;
+import java.util.Random;
+
+public class Calculations {
+
+    public static LinkedList miniLotto() {
+        LinkedList linkedList = new LinkedList();
+        Random generator = new Random();
+
+        for (int i = 0; i < 5; i++) {
+
+            linkedList.add(generator.nextInt(41));
+        }
+        return linkedList;
+    }
+
+    public static LinkedList bigLotto() {
+        LinkedList linkedList = new LinkedList();
+        Random generator = new Random();
+
+        for (int i = 0; i < 6; i++) {
+
+            linkedList.add(generator.nextInt(50));
+        }
+        return linkedList;
+    }
+
+    public void multiLotto() {
+    }
+
+    public void eurojackpotLotto() {
+    }
+
+
+}
---
+++ src/sample/Controller.java
@@ -0,0 +1,49 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.util.Random;
+
+
+public class Controller {
+
+    @FXML
+    Button big;
+
+    @FXML
+    TextField bigText;
+
+    @FXML
+    Button mini;
+
+    @FXML
+    TextField miniText;
+
+    @FXML
+    Button multi;
+
+    @FXML
+    Button mText;
+
+    @FXML
+    Button eurojackpot;
+
+    @FXML
+    Button euroText;
+
+    public void initialize() {
+
+        big.setOnAction(event -> {
+            bigText.setText(String.valueOf(Calculations.bigLotto()));
+        });
+
+        mini.setOnAction(event -> {
+            miniText.setText(String.valueOf(Calculations.miniLotto()));
+        });
+
+    }
+
+
+}
