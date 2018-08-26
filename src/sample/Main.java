5687421ea245e6d91bedd17c1b59c208a30c576a
Grzegorz_B <budnikgrzegorz@interia.pl>
2018-08-26 15:29:01 +0200

    Last commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    int a;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("POLISH LOTTO MACHINE");
         Scene scene = new Scene(root, 701, 600);
+        primaryStage.getIcons().add(new Image("image/lotto_z_bialym_obrysem.png"));
         primaryStage.setScene(scene);
         scene.getStylesheets().add("style/styleRoot.css");
         primaryStage.show();
